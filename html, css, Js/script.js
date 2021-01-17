60ed4c0438351521521f91a4c29cf2897715ce97
isaac Groisman <grisman13@hotmail.com>
2021-01-17 23:36:00 +0200

    added html files

---
+++ html, css, Js/script.js
@@ -0,0 +1,19 @@
+/***********
+Making placeholder star specifically red 
+****************/
+
+
+$('.palceholder').click(function () {
+    $(this).siblings('input').focus();
+});
+$('.form-control').focus(function () {
+    $(this).siblings('.palceholder').hide();
+});
+$('.form-control').blur(function () {
+    var $this = $(this);
+    if ($this.val().length == 0)
+        $(this).siblings('.palceholder').show();
+});
+$('.form-control').blur();
+
+
